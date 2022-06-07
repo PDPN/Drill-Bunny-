523bd7ba4dd0ab712270809a3b46c1ab37af92d2
PDPN <69457861+PDPN@users.noreply.github.com>
2022-06-07 21:55:34 +0530

    Add files via upload

---
+++ Pop-Pop-Win-master/test/test_util.dart
@@ -0,0 +1,22 @@
+import 'package:pop_pop_win/src/game.dart';
+import 'package:test/test.dart';
+
+// This grid
+// XXXXX2
+// X7X8X3
+// X5XXX2
+// X32321
+// 110000
+
+const sampleField = <int?>[
+  null, null, null, null, null, 2, // row
+  null, 7, null, 8, null, 3,
+  null, 5, null, null, null, 2,
+  null, 3, 2, 3, 2, 1,
+  1, 1, 0, 0, 0, 0
+];
+
+Field getSampleField() =>
+    Field.fromSquares(6, 5, List<bool>.from(sampleField.map((x) => x == null)));
+
+final throwsAssertionError = throwsA(const TypeMatcher<AssertionError>());
